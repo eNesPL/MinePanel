fc97c5da433ebca2f7328e60febc7315a0e6b7be
Pollond <mr.volvers@gmal.com>
2020-06-07 21:54:43 +0200

    Test4

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="utf-8">
     <title>MinePanel</title>
     <link rel="stylesheet" href="main.css">
-    <!--<meta http-equiv="refresh" content="30">-->
+    <meta http-equiv="refresh" content="30">
 </head>
 <body>
+<div class="hedear">
+     <div class="logo">Logo</div>
 
+</div>
 </body>
 </html>
