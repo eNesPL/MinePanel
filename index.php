6014279a5ee7df97ac0401c5dd5efaef0dc22f61
eNesPL <enescraft@gmail.com>
2020-06-07 21:24:12 +0200

    ;-;

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>MinePanel</title>
+    <link rel="stylesheet" href="main.css">
+    <!--<meta http-equiv="refresh" content="30">-->
+</head>
+<body>
+
+</body>
+</html>
