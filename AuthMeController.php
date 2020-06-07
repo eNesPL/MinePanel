1778b8c9e937e1b38e438f17195925b85b0006aa
eNesPL <enescraft@gmail.com>
2020-06-07 21:03:04 +0200

    ;-;

--- AuthMeController.php
+++ AuthMeController.php
@@ -95,7 +95,7 @@ abstract class AuthMeController {
      */
     private function getAuthmeMySqli() {
         // CHANGE YOUR DATABASE DETAILS HERE BELOW: host, user, password, database name
-        $mysqli = new mysqli('localhost', 'root', '', 'authme');
+        $mysqli = new mysqli('localhost', 'authme', 'authme', 'authme');
         if (mysqli_connect_error()) {
             printf('Could not connect to AuthMe database. Errno: %d, error: "%s"',
                 mysqli_connect_errno(), mysqli_connect_error());
