8bb995e7f20599d465678d9bfdddbd09fe14a19e
eNesPL <enescraft@gmail.com>
2020-06-10 18:40:58 +0200

    ;-;

--- index.php
+++ index.php
@@ -3,12 +3,65 @@
 <head>
     <meta charset="utf-8">
     <title>MinePanel</title>
-    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="/js/bootstrap.js"></script>
-    <!--<meta http-equiv="refresh" content="30">-->
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<div class="wrapper">
+    <div class="logo">
+        <img src="img/logo.png" alt="Logo">
+    </div>
+    <div class="nav">
+        <ol>
+            <li><a href="#">Text1</a></li>
+            <li><a href="#">Text2</a></li>
+            <li><a href="#">Text3</a></li>
+            <li><a href="#">Text4</a></li>
+        </ol>
+    </div>
+    <div class="container">
+        <div class="main">
+            <div class="article">
+                <div class="img">Tytuł</div>
+            </div>
+            <div class="article">
+                <div class="img">Tytuł</div>
+            </div>
+            <div class="article">
+                <div class="img">Tytuł</div>
+            </div>
+            <div class="article">
+                <div class="img">Tytuł</div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="logowanie">
+                <input class="input" type="text" placeholder="Login"><br/>
+                <input class="input" type="password" placeholder="Hasło"><br/>
+                <input class="input" type="submit" value="Zaloguj"><br/>
+                <div class="txt"><a href="#">Zapomniałeś hasła ?</a></div>
+            </div>
+            <div class="ranking">
+                <div class="rankighead"><h1>Top 5 Graczy</h1></div>
+                <div class="pozrank">Test1</div>
+                <div class="pozrank">Test2</div>
+                <div class="pozrank">Test3</div>
+                <div class="pozrank">Test4</div>
+                <div class="pozrank">Test5</div>
+            </div>
+            <div class="ranking">
+                <div class="rankighead"><h1>Top 5 Online</h1></div>
+                <div class="pozrank">Test1</div>
+                <div class="pozrank">Test2</div>
+                <div class="pozrank">Test3</div>
+                <div class="pozrank">Test4</div>
+                <div class="pozrank">Test5</div>
+            </div>
+        </div>
+        <div style="clear: both"></div>
+    </div>
+</div>
 </body>
 </html>
