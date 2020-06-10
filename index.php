48302e06c01a452b29e651d89ffad969ca3fa597
Pollond <mr.volvers@gmal.com>
2020-06-10 12:15:12 +0200

    UPP#2 10/6/20

--- index.php
+++ index.php
@@ -9,21 +9,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="wrapper">
-        <a href="index.php"><div class="logo">
-           <div class="block1"><img src="img/logo.png"></div>
-            <div class="block2"><h1 class="h1">MinePanel</h1><h5 class="h5">Coś więcej niż minecraft</h5></div>
-        </div></a>
-        <div class="menu">
-            <ul class="nav">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About Us</a></li>
-                <li><a href="#clients">Our Clients</a></li>
-                <li><a href="#contact">Contact Us</a></li>
-            </ul>
+<div class="wrapper">
+        <div class="logo">
+            <img src="img/logo.png">
         </div>
-        <div class="main">3</div>
-        <div class="footer">4</div>
-    </div>
+        <div class="nav">
+
+        </div>
+        <div class="container">
+
+        </div>
+</div>
 </body>
 </html>
