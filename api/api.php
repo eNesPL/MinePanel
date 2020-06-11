1c510410614798c14ccdb56c90979d6b6170c3e8
eNesPL <enescraft@gmail.com>
2020-06-11 16:16:08 +0200

    apitest

--- api/api.php
+++ api/api.php
@@ -1,2 +1,13 @@
 <?php
-
+require './config/sesja.php';
+function getOnline(){
+    $sql="select username from authme where isLogged==1";
+    if (isset($conn)) {
+        $result=mysqli_query($conn,$sql);
+        while($row=mysqli_fetch_row($result)){
+            echo $row;
+        }
+    }else{
+        echo"error";
+    }
+}
