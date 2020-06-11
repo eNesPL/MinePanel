a3be3d82ace98e4e61641830375427f3dc20bc59
eNesPL <enescraft@gmail.com>
2020-06-11 17:00:18 +0200

    php

--- api/api.php
+++ api/api.php
@@ -5,6 +5,7 @@ function getOnline($conn){
     $sql='select username from authme.authme where isLogged = 1';
 
         $result = mysqli_query($conn, $sql);
+        echo $result;
         return mysqli_fetch_array($result);
 }
 echo getOnline($conn);
