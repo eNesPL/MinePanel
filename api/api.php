c7fb4d87363428015d3f50e6004bcf5e64ce086c
eNesPL <enescraft@gmail.com>
2020-06-11 17:14:13 +0200

    php

--- api/api.php
+++ api/api.php
@@ -3,15 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include("../config/sesja.php");
-$user = posix_getpwuid(posix_geteuid());
-var_dump($user);
+
 function getOnline($conn){
 
     $sql='select username from authme.authme where isLogged = 1';
 
         $result = mysqli_query($conn, $sql);
-        echo $result;
-        return mysqli_fetch_array($result);
+        $row = mysqli_fetch_array($result);
+        return $row;
 }
 $arr = getOnline($conn);
 foreach($arr as $nick){
