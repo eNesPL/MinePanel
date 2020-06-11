af621e919a2fb8fd26116ce7ef7d96f6c5c792c8
eNesPL <enescraft@gmail.com>
2020-06-11 16:40:51 +0200

    php

--- index.php
+++ index.php
@@ -48,7 +48,8 @@
                 <input class="input" type="submit" name="action" value="Zaloguj"><br/>
                 </form>
                 <div class="txt"><a href="#">Zapomniałeś hasła ?</a></div>
-                <?;}elseif(isset($_SESSION['login']) && $_SESSION['loged']=1){
+                <?;}
+                if(isset($_SESSION['login']) && $_SESSION['loged']=1){
                 echo "Witaj ".$_SESSION['login'];
                 ?>
                 <form action="config/login.php" method="post">
